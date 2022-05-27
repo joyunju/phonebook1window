158a06d899fbd3256e0171f4a63d412b6e68a78b
joyunju <whdbswn1113@naver.com>
2022-05-27 15:11:26 +0900

    phoneDao

--- src/com/javaex/dao/PhoneDao.java
+++ src/com/javaex/dao/PhoneDao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.javaex.Vo.PersonVo;
+import com.javaex.vo.PersonVo;
 
 public class PhoneDao {
 
@@ -176,7 +176,7 @@ public class PhoneDao {
 			count = pstmt.executeUpdate(); // 쿼리문 실행
 
 			// 4.결과처리
-			// System.out.println(count + "건 수정되었습니다.");
+			System.out.println(count + "건 수정되었습니다.");
 
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
@@ -211,6 +211,51 @@ public class PhoneDao {
 
 		close();
 		return count;
+	}
+
+	// updateForm 사용 위해
+	public PersonVo getPerson(int personId) {
+		PersonVo personVo = null;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// SQL문 준비
+			String query = "";
+			query += " select  person_id, ";
+			query += "         name, ";
+			query += "         hp, ";
+			query += "         company ";
+			query += " from person ";
+			query += " where person_id = ? ";
+
+			// 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, personId);
+
+			// 실행
+			rs = pstmt.executeQuery();
+
+			// 4.결과처리
+			while (rs.next()) {
+
+				int id = rs.getInt("person_id");
+				String name = rs.getString("name");
+				String hp = rs.getString("hp");
+				String company = rs.getString("company");
+
+				personVo = new PersonVo(id, name, hp, company);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		this.close();
+
+		return personVo;
 	}
 
 }
